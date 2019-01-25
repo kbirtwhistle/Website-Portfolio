912c102bd4a7da7d4f554ae1f59cf98309ffa45d
kbirtwhistle <46375223+kbirtwhistle@users.noreply.github.com>
2019-01-25 14:57:33 +0000

    Starter JS testing

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    
+    $('.js--welcome--to--synapse').click(function () {
+        $('html, body').animate({scrollTop: $('js--section--features').offset().top}, 1000);
+        
+        
+        
+    
+                              
+                              
+});
+    
+    
+
+});
